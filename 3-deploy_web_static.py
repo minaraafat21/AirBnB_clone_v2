7dc45c7aa9cadfdd0f9ae9278573f6055d3bf30f
mina <minaraafatemil14@gmail.com>
2024-03-07 17:10:26 +0200

    MON

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,75 +1,64 @@
 #!/usr/bin/python3
-"""Fabric script (based on the file 2-do_deploy_web_static.py) that creates and
-distributes an archive to your web servers, using the function deploy"""
-import os
+"""
+Fabric script (based on the file 2-do_deploy_web_static.py) that creates and
+distributes an archive to your web servers, using the function deploy
+"""
+from fabric.api import local, env, run, put, hosts
 from datetime import datetime
-from fabric.api import *
-
+import os
 
 env.hosts = ['34.224.15.155', '	100.25.165.235']
 
 
 def do_pack():
-    """Creates archive from web_static directory"""
-    local("mkdir -p versions")
-    file = 'versions/web_static_{}.tgz'\
-        .format(datetime.strftime(datetime.now(), "%Y%m%d%I%M%S"))
-    comp = 'tar -cvzf {} web_static'.format(file)
-    tar_file = local(comp)
-    if tar_file.failed:
+    """
+    create the archive file with the contents of the web_static folder
+    and return the archive path if the archive has been correctly generated
+    otherwise return None
+    """
+    datetime_str = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_name = "web_static_{}.tgz".format(datetime_str)
+    try:
+        local('mkdir -p versions')
+        local('tar -cvzf versions/{} web_static'.format(file_name))
+        return "versions/{}".format(file_name)
+    except:
         return None
-    else:
-        return file
 
 
 def do_deploy(archive_path):
-    """Deploys an archive"""
+    """
+    upload the archive to the /tmp/ directory of the web server
+    uncompress the archive
+    delete the archive from the web server
+    handle symbolic links
+    return True is all operations went well, False otherwise
+    """
     if not os.path.exists(archive_path):
         return False
-    arch = archive_path.split('/')[1]
-    name = arch.split('.')[0]
-    tar_file = put(archive_path, '/tmp/{}'.format(arch))
-    if tar_file.failed:
+    try:
+        put(archive_path, '/tmp/')
+        file_name = archive_path.split('/')[-1]
+        file_name_noext = file_name.split('.')[0]
+        new_folder = '/data/web_static/releases/' + file_name_noext + '/'
+        run('sudo mkdir -p {}'.format(new_folder))
+        run('sudo tar -xzf /tmp/{} -C {}'.format(file_name, new_folder))
+        run('sudo rm /tmp/{}'.format(file_name))
+        run('sudo mv {}web_static/* {}'.format(new_folder, new_folder))
+        run('sudo rm -rf {}web_static'.format(new_folder))
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s {} /data/web_static/current'.format(new_folder))
+        print("New version deployed!")
+        return True
+    except:
         return False
-    tar_file = run('mkdir -p /data/web_static/releases/{}'.format(name))
-    if tar_file.failed:
-        return False
-    tar_file = run(
-        'tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
-        .format(arch, name))
-    if tar_file.failed:
-        return False
-    tar_file = run('rm /tmp/{}'.format(arch))
-    if tar_file.failed:
-        return False
-    comp = 'mv /data/web_static/releases/{0}/web_static/*'
-    comp += ' /data/web_static/releases/{0}/'
-    tar_file = run(comp.format(name))
-    if tar_file.failed:
-        return False
-    tar_file = run(
-                'rm -rf /data/web_static/releases/{}/web_static'
-                .format(name))
-    if tar_file.failed:
-        return False
-    tar_file = run('rm -rf /data/web_static/current')
-    if tar_file.failed:
-        return False
-    tar_file = run(
-            'ln -s /data/web_static/releases/{}/ /data/web_static/current'
-            .format(name))
-    if tar_file.failed:
-        return False
-    print('New version deployed!')
-    return True
 
 
 def deploy():
-    """ Fabric script (based on the file 2-do_deploy_web_static.py)
-    that creates and distributes an archive to your web servers,
-    using the function deploy"""
-    archive = do_pack()
-    if archive is None:
+    """calls do_pack() and do_deploy()"""
+    try:
+        archive_path = do_pack()
+        status = do_deploy(archive_path)
+        return status
+    except:
         return False
-    tar_file = do_deploy(archive)
-    return tar_file
